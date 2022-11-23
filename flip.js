cfb96ad96bf4f1e0701fcd9933e03802cd62c972
Yamanayya79 <yamanayya6603@gmail.com>
2022-11-23 18:52:26 +0530

    flipkart

---
+++ flip.js
@@ -0,0 +1,166 @@
+//array of object
+const Electonicproduct = [
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/jlzhci80/printer/m/f/g/canon-ts207-original-imaf8zvuejuqth5b.jpeg?q=70",
+        Name: "Printer",
+        price: 60000,
+        brand: "Canon"
+    },
+
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Top deals of camera accessori",
+        price: 630,
+        brand: "canon,sony, FujiFlim"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/kynb6vk0/monitor/1/n/s/lf22t354fhwxxl-full-hd-22-lf22t354fhwxxl-samsung-original-imagats2rjbg9uhv.jpeg?q=70",
+        Name: "Top mirror less camera",
+        price: 50000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Monitor",
+        price: 9999,
+        brand: "Samsung"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/hair-dryer/c/t/g/philips-hp8100-46-original-imaemymzuxrnzfjb.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "best of hair",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    }
+]
+const Electonicproduct1 = [
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
+        Name: "Soft toys",
+        price: 5000,
+        brand: "Primium non-geared cycle"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/kz5vwy80/helmet/o/4/k/-original-imagb8azfdthjhqr.jpeg?q=70",
+        Name: "Rider Helmets",
+        price: 6000,
+        brand: " Vega steelibird"
+    },
+
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Top deals of camera accessori",
+        price: 630,
+        brand: "canon,sony, FujiFlim"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/kynb6vk0/monitor/1/n/s/lf22t354fhwxxl-full-hd-22-lf22t354fhwxxl-samsung-original-imagats2rjbg9uhv.jpeg?q=70",
+        Name: "Top mirror less camera",
+        price: 50000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Monitor",
+        price: 9999,
+        brand: "Samsung"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/hair-dryer/c/t/g/philips-hp8100-46-original-imaemymzuxrnzfjb.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "best of hair",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    }
+]
+let = renderproduct = "";
+let cardproductcontiner = document.getElementById('card-product-continer')
+for (let i = 0; i < Electonicproduct.length; i++) {
+    console.log(Electonicproduct[i])
+    renderproduct += `
+    <div class="card-product">
+    <div class="productImgcontiner">
+    <img src="${Electonicproduct[i].imgurl}">
+    </div>
+    <h3 class="card-product-name">${Electonicproduct[i].Name}</h3>
+   <p class="card-product-price"> From ₹ ${Electonicproduct[i].price}</p>
+                    <p class="card-product-dec">${Electonicproduct[i].brand}</p>
+
+    </div>
+    `
+}
+cardproductcontiner.innerHTML = renderproduct;
+let cardbtnright = document.getElementById("card-btn-right")
+let cardbtnleft = document.getElementById("card-btn-left")
+cardbtnleft.style.display = "none"
+cardbtnright.addEventListener("click", function () {
+    cardproductcontiner.style.transform = "translatex(-30%)"
+    cardbtnright.style.display = "none"
+    cardbtnleft.style.display = "block"
+})
+cardbtnleft.addEventListener("click", function () {
+    cardproductcontiner.style.transform = "translatex(0%)"
+    cardbtnright.style.display = "block"
+    cardbtnleft.style.display = "none"
+
+})
+// ===========2nd row======================
+let = renderproduct = "";
+let cardproductcontiner1 = document.getElementById('card-product-continer1')
+for (let i = 0; i < Electonicproduct1.length; i++) {
+    console.log(Electonicproduct1[i])
+    renderproduct += `
+    <div class="card-product">
+    <div class="productImgcontiner">
+    <img src="${Electonicproduct1[i].imgurl}">
+    </div>
+    <h3 class="card-product-name">${Electonicproduct1[i].Name}</h3>
+   <p class="card-product-price"> From ₹ ${Electonicproduct1[i].price}</p>
+                    <p class="card-product-dec">${Electonicproduct1[i].brand}</p>
+
+    </div>
+    `
+}
+cardproductcontiner1.innerHTML = renderproduct;
+let cardbtnright1 = document.getElementById("card-btn-right")
+let cardbtnleft1 = document.getElementById("card-btn-left")
+cardbtnleft1.style.display = "none"
+cardbtnright1.addEventListener("click", function () {
+    cardproductcontiner1.style.transform = "translatex(-30%)"
+    cardbtnright1.style.display = "none"
+    cardbtnleft1.style.display = "block"
+})
+cardbtnlef1t.addEventListener("click", function () {
+    cardproductcontiner1.style.transform = "translatex(0%)"
+    cardbtnright1.style.display = "block"
+    cardbtnleft1.style.display = "none"
+
+})
