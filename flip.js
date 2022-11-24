76194805b81cb597a035fb52a4541c97456c80b3
Yamanayya79 <yamanayya6603@gmail.com>
2022-11-24 18:34:43 +0530

    flip kart

--- flip.js
+++ flip.js
@@ -101,6 +101,57 @@ const Electonicproduct1 = [
         brand: "Hp"
     }
 ]
+const Electonicproduct2 = [
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
+        Name: "Soft toys",
+        price: 5000,
+        brand: "Primium non-geared cycle"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/kz5vwy80/helmet/o/4/k/-original-imagb8azfdthjhqr.jpeg?q=70",
+        Name: "Rider Helmets",
+        price: 6000,
+        brand: " Vega steelibird"
+    },
+
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Top deals of camera accessori",
+        price: 630,
+        brand: "canon,sony, FujiFlim"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/kynb6vk0/monitor/1/n/s/lf22t354fhwxxl-full-hd-22-lf22t354fhwxxl-samsung-original-imagats2rjbg9uhv.jpeg?q=70",
+        Name: "Top mirror less camera",
+        price: 50000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Monitor",
+        price: 9999,
+        brand: "Samsung"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/hair-dryer/c/t/g/philips-hp8100-46-original-imaemymzuxrnzfjb.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "best of hair",
+        price: 5000,
+        brand: "Hp"
+    },
+    {
+        imgurl: "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        Name: "Printer",
+        price: 5000,
+        brand: "Hp"
+    }
+]
 let = renderproduct = "";
 let cardproductcontiner = document.getElementById('card-product-continer')
 for (let i = 0; i < Electonicproduct.length; i++) {
@@ -158,9 +209,51 @@ cardbtnright1.addEventListener("click", function () {
     cardbtnright1.style.display = "none"
     cardbtnleft1.style.display = "block"
 })
-cardbtnlef1t.addEventListener("click", function () {
+cardbtnleft1.addEventListener("click", function () {
     cardproductcontiner1.style.transform = "translatex(0%)"
     cardbtnright1.style.display = "block"
     cardbtnleft1.style.display = "none"
 
 })
+// ===========3rd=========
+let = renderproduct = "";
+let cardproductcontiner2 = document.getElementById('card-product-continer2')
+for (let i = 0; i < Electonicproduct2.length; i++) {
+    console.log(Electonicproduct2[i])
+    renderproduct += `
+    <div class="card-product">
+    <div class="productImgcontiner">
+    <img src="${Electonicproduct2[i].imgurl}">
+    </div>
+    <h3 class="card-product-name">${Electonicproduct2[i].Name}</h3>
+   <p class="card-product-price"> From ₹ ${Electonicproduct2[i].price}</p>
+                    <p class="card-product-dec">${Electonicproduct2[i].brand}</p>
+
+    </div>
+    `
+}
+cardproductcontiner2.innerHTML = renderproduct;
+let cardbtnright2 = document.getElementById("card-btn-right")
+let cardbtnleft2 = document.getElementById("card-btn-left")
+cardbtnleft2.style.display = "none"
+cardbtnright2s.addEventListener("click", function () {
+    cardproductcontiner2.style.transform = "translatex(-30%)"
+    cardbtnright2.style.display = "none"
+    cardbtnleft2.style.display = "block"
+})
+cardbtnleft1.addEventListener("click", function () {
+    cardproductcontiner2.style.transform = "translatex(0%)"
+    cardbtnright2.style.display = "block"
+    cardbtnleft2.style.display = "none"
+
+})
+
+// =============================coupon page============================
+function loadcoupon(){
+    document.getElementById('coupon').style.visibility='visible';
+    document.getElementById('body').style.opacity='0.7';
+}
+function closecoupon(){
+    document.getElementById('coupon').style.visibility='hidden';
+    // document.getElementById('body').style.opacity='0.7';
+}
